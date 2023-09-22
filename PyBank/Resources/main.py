fbe1a1f3d0ddfb2f7c8bcc65a69174dd328f4e68
そり屋野맽 <143652580+ss0ryan0@users.noreply.github.com>
2023-09-22 17:56:42 -0400

    9/22 17:56 main.py

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -3,3 +3,7 @@ import csv
 csvpath = os.path.join('python-challenge', 'PyBank', 'Resources', 'budget_data.csv')
 with open (csvpath, encoding='UTF-8') as csvfile:
   csvreader = csv.reader(csvfile, delimiter=', ')
+  for column in csvreader,
+    total_months = len(column)  
+    print('Total Months:' + total_months)
+    
