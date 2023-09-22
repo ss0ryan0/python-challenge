29c9fee46244051d1a041ccaf531ea5d930edb35
そり屋野맽 <143652580+ss0ryan0@users.noreply.github.com>
2023-09-22 17:45:30 -0400

    9/22 17:45 main.py

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -1,3 +1,5 @@
 import os
 import csv
-
+csvpath = os.path.join('python-challenge', 'PyBank', 'Resources', 'budget_data.csv')
+with open (csvpath, encoding='UTF-8') as csvfile:
+  csvreader = csv.reader(csvfile, delimiter=', ')
